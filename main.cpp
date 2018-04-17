8d739396599e430b997ba6115cc293e7b0883263
Andrey Shaj <kolobrod2002@yandex.ru>
2018-04-17 14:22:20 +0300

    Without tuple and documentation

--- main.cpp
+++ main.cpp
@@ -21,11 +21,12 @@ int main (int argc, char *argv[])
 	std::cout << my::print_ip(char(-1)) << std::endl;
 	std::cout << my::print_ip(short(0)) << std::endl;
 	std::cout << my::print_ip(int(2130706433)) << std::endl;
-	std::cout << my::print_ip(long(8875824491850138409)) << std::endl;
+	std::cout << my::print_ip((long long)(8875824491850138409)) << std::endl;
+	std::cout << my::print_ip((long long)(0xf0f1f2f3)) << std::endl;
 	std::cout << my::print_ip(std::string("ip: 1.2.3.4")) << std::endl;
-	std::cout << my::print_ip(std::vector<int>{10, 20, 30, 40, 50, 60}) << std::endl;
-	std::cout << my::print_ip(std::list<int>{95, 85, 75, 65, 55, 45, 1024}) << std::endl;
-	std::cout << my::print_ip(std::make_tuple(1024, 2048, 4096, 10)) << std::endl;
+	std::cout << my::print_ip(std::vector<char>{-10, 20, -30, 40, -10, 20}) << std::endl;
+	std::cout << my::print_ip(std::list<int>{2130706433, 85, 2130706433, 65, 2130706433, 45, 255}) << std::endl;
+	// std::cout << my::print_ip(std::make_tuple(1024, 2048, 4096, 10)) << std::endl;
 
 	return 0;
 }
--- printer.h
+++ printer.h
@@ -4,20 +4,90 @@
 
 
 #include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
 
 
 namespace my
 {
 
+template<class T>
+struct type_is
+{
+	using type = T;
+};
+
+template<typename T, T v>
+struct integral_constant
+{
+	static constexpr T value = v;
+	using type = integral_constant;
+};
+
+template <bool B>
+using bool_constant = integral_constant<bool, B>;
+
+using true_type = bool_constant<true>;
+using false_type = bool_constant<false>;
+
+
+template<bool B, class T>
+struct enable_if : type_is<T> {};
+
+template<class T>
+struct enable_if<false, T> {};
+
+
+template<typename T, typename ... Args>
+struct is_one_of;
+
+template<typename T>
+struct is_one_of<T> : false_type {};
+
+template<typename T, typename ... Args>
+struct is_one_of<T, T, Args...> : true_type {};
+
+template<typename T, typename U, typename ... Args>
+struct is_one_of<T, U, Args...> : is_one_of<T, Args...> {};
+
 
 
 template<typename T>
-std::string print_ip(T val)
+typename enable_if<std::is_integral<T>::value, std::string>::type print_ip(const T &val)
 {
-	return std::string{"0.0"};
+	std::ostringstream s;
+	auto pLow = reinterpret_cast<const unsigned char*>(&val);
+	auto pHigh = reinterpret_cast<const unsigned char*>(&val) + sizeof(val);
+
+	if(sizeof(val) == 1)
+	{
+		s << static_cast<unsigned int>(*pLow);
+	}
+	else
+	{
+		std::reverse_copy(pLow+1, pHigh, std::ostream_iterator<unsigned int>(s, "."));
+		s << static_cast<unsigned int>(*pLow);
+	}
+	return s.str();
 }
 
+template<typename T>
+typename enable_if<is_one_of<T, std::string>::value, std::string>::type print_ip(const T &val)
+{
+	return static_cast<std::string>(val);
+}
 
-
+template<class T>
+typename enable_if<is_one_of<T, std::vector<typename T::value_type>, std::list<typename T::value_type>>::value,
+		std::string>::type print_ip(const T &val)
+{
+	std::ostringstream s;
+	for(const auto &it: val)
+	{
+		s << print_ip(it) << std::endl;
+	}
+	return s.str();
+}		
 
 }
